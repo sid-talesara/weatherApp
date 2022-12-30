8304b097d31e45f9db1c0b5b11d764645461f644
sid-talesara <2020pcecssiddharth207@poornima.org>
2022-12-30 22:58:28 +0530

    weather app made with bootstrap and rapid api

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
- 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7e3eaf5fd4msh0d4455f140ae1f4p127539jsnace134e8d573",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWeather = (city) => {
+  fetch(
+    `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      cityName.innerHTML = city;
+      console.log(response);
+      cloud_pct.innerHTML = response.cloud_pct;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      max_temp.innerHTML = response.max_temp;
+      min_temp.innerHTML = response.min_temp;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+      temp.innerHTML = response.temp;
+      wind_degrees.innerHTML = response.wind_degrees;
+      //   wind_speed.innerHTML = response.wind_speed;
+      tempMain.innerHTML = response.temp;
+      humidityMain.innerHTML = response.humidity;
+      wind_speedMain.innerHTML = response.wind_speed;
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Delhi");
